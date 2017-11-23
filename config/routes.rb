b662766277d84a76bc7c188a39b5f4b229f666a4
charlie  lee <ctlee9096@gmail.com>
2017-11-23 02:32:15 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :artists do
       resources :songs, only: [:index, :show]
     end
-    # resources :songs
+     resources :songs
 
     scope '/admin', module: 'admin' do
      resources :preferences, only: [:index]
